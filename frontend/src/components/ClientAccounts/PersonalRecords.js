ee76fe7d5863cd99d0dcf0a2e23b89c70e893e52
RealLionelMuhire <muhirelionel@gmail.com>
2023-09-15 14:24:18 +0200

    adding clients codes

---
+++ frontend/src/components/ClientAccounts/PersonalRecords.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PersonalRecords() {
+  return (
+    <div>
+      {/* Client Accounts: Personal Records */}
+      <h2>Personal Records</h2>
+      {/* Add UI elements for managing personal records */}
+    </div>
+  );
+}
+
+export default PersonalRecords;
+
