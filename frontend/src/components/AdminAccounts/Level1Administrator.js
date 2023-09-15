fbd5b690b87f45a96aa148febe7a5ffc724fe380
RealLionelMuhire <muhirelionel@gmail.com>
2023-09-15 14:17:31 +0200

    Admin_Acc

---
+++ frontend/src/components/AdminAccounts/Level1Administrator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Level1Administrator() {
+  return (
+    <div>
+      {/* Administrator Accounts: Level 1 Administrator */}
+      <h2>Level 1 Administrator</h2>
+      {/* Add UI elements specific to Level 1 Administrator */}
+    </div>
+  );
+}
+
+export default Level1Administrator;
+
---
+++ frontend/src/components/AdminAccounts/Level2Administrator.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Level2Administrator() {
+  return (
+    <div>
+      {/* Administrator Accounts: Level 2 Administrator */}
+      <h2>Level 2 Administrator</h2>
+      {/* Add UI elements specific to Level 2 Administrator */}
+    </div>
+  );
+}
+
+export default Level2Administrator;
+
