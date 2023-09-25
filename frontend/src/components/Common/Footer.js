30de8b090802212dc437c6791947d24ffbd59028
RealLionelMuhire <muhirelionel@gmail.com>
2023-09-25 15:48:59 +0200

    adding some styles on footer

--- frontend/src/components/Common/Footer.js
+++ frontend/src/components/Common/Footer.js
@@ -2,12 +2,15 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="footer">
-      <div className="container text-center">
-        <p>&copy; {new Date().getFullYear()} ML Corporate Services</p>
-      </div>
-    </footer>
+    <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="https://mlcorporateservices.com/" target="_blank" class="nav-link px-2 text-body-secondary">Home</a></li>
+      <li class="nav-item"><a href="https://jovian.ai/learn" target="_blank" class="nav-link px-2 text-body-secondary">Book video Call Appointemnet</a></li>
+      <li class="nav-item"><a href="https://jovian.ai/events" target="_blank" class="nav-link px-2 text-body-secondary">Events</a></li>
+      <li class="nav-item"><a href="https://mlcorporateservices.com/" target="_blank" class="nav-link px-2 text-body-secondary">About</a></li>
+    </ul>
+    <p class="text-center text-body-secondary">&copy; {new Date().getFullYear()} ML_Careeres, Inc</p>
+  </footer>
   );
 }
-
 export default Footer;
--- frontend/src/components/Common/Header.js
+++ frontend/src/components/Common/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header className="header">
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">ML Corporate Services</a>
+        <a className="navbar-brand" href="https://mlcorporateservices.com">ML Corporate Services</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
