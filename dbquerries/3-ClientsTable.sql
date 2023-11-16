0fa9108ad83ee5364ae010901bba6fffe60d4701
RealLionelMuhire <muhirelionel@gmail.com>
2023-11-17 00:35:42 +0200

    updating client table

--- dbquerries/3-ClientsTable.sql
+++ dbquerries/3-ClientsTable.sql
@@ -1,7 +1,23 @@
 CREATE TABLE Clients (
     ClientID INT PRIMARY KEY AUTO_INCREMENT,
     UserID INT,
-    ClientStatus VARCHAR(20),
+    ClientStatus VARCHAR(20) NOT NULL,
+    ClientName VARCHAR(100) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
+    CompanyName VARCHAR(100) NOT NULL,
+    NationalID VARCHAR(20) NOT NULL,
+    PassportNumber VARCHAR(20),
+    PassportExpirationDate DATE,
+    TINNumber VARCHAR(20) NOT NULL,
+    RegistrationIndexNumber VARCHAR(20) NOT NULL,
+    OwnerShareholderID VARCHAR(20) NOT NULL,
+    StageOfCompany VARCHAR(50) NOT NULL,
+    CategoryOfCompany VARCHAR(50) NOT NULL,
+    CopyOfIDOrPassport BLOB NOT NULL,
+    RegistrationCertificate BLOB NOT NULL,
+    TaxCertificate BLOB NOT NULL,
+    ProofOfResidence BLOB NOT NULL,
+    Shareholder BLOB NOT NULL,
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
