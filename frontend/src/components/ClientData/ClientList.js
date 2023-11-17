7dd1b3574ab3a3b0523006f6110f02876ecf62d3
RealLionelMuhire <muhirelionel@gmail.com>
2023-11-17 11:30:17 +0200

    adding frontend connect to backend

--- frontend/src/components/ClientData/ClientList.js
+++ frontend/src/components/ClientData/ClientList.js
@@ -1,28 +1,33 @@
 // src/components/ClientList.js
-
 import React, { useState, useEffect } from 'react';
+import { getClients } from '../../../src/services/api';
 
-const ClientList = () => {
+function ClientList() {
   const [clients, setClients] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/clients')  // Update with your backend URL
-      .then(response => response.json())
-      .then(data => setClients(data.clients))
-      .catch(error => console.error('Error fetching clients:', error));
+    // Fetch the list of clients when the component mounts
+    getClients()
+      .then((response) => {
+        setClients(response.data); // Assuming your API returns an array of clients
+      })
+      .catch((error) => {
+        console.error('Error fetching clients:', error);
+      });
   }, []);
 
   return (
     <div>
-      <h2>Clients</h2>
+      <h2>Client List</h2>
       <ul>
-        {clients.map(client => (
-          <li key={client.id}>{client.username}</li>
+        {clients.map((client) => (
+          <li key={client.id}>{client.fullName}</li>
+          // Replace 'id' and 'fullName' with the actual fields returned by your API
         ))}
       </ul>
     </div>
   );
-};
+}
 
 export default ClientList;
 
