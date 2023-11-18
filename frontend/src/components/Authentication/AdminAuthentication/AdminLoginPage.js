3e00ffea5044d24979aa74a1f812b970b5ed38fc
RealLionelMuhire <muhirelionel@gmail.com>
2023-11-18 23:24:30 +0200

    updaing handle login

--- frontend/src/components/Authentication/AdminAuthentication/AdminLoginPage.js
+++ frontend/src/components/Authentication/AdminAuthentication/AdminLoginPage.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import Header from '../../Common/Header';
 import Footer from '../../Common/Footer';
+import { loginUser } from '../../services/api'; // Import the API service
 import './AdminLoginPage.css';
 
 function AdminLoginPage() {
@@ -22,8 +23,7 @@ function AdminLoginPage() {
     });
   };
 
-  const handleLogin = () => {
-    // Perform your login logic here
+  const handleLogin = async () => {
     // Check if the identifier and password are provided
     if (!formData.identifier || !formData.password) {
       setError('Please provide both username/email and password.');
@@ -33,10 +33,22 @@ function AdminLoginPage() {
     // Clear previous errors
     setError('');
 
-    // Your login logic goes here
+    try {
+      // Call the API service for login
+      const token = await loginUser({
+        identifier: formData.identifier,
+        password: formData.password,
+      });
 
-    // Simulate a successful login for demonstration purposes
-    navigate('/admin-dashboard');
+      // Store the user token in local storage or a cookie
+      localStorage.setItem('userToken', token);
+
+      // Simulate a successful login for demonstration purposes
+      navigate('/admin-dashboard');
+    } catch (error) {
+      // Handle login errors (e.g., display an error message)
+      setError(error.message);
+    }
   };
 
   return (
@@ -49,37 +61,7 @@ function AdminLoginPage() {
               <h2 className="logo">WELCOME TO ML CORPORATE SERVICES</h2>
               <p>For administrators only</p>
               <form id="admin_login_form" action="/auth/admin_sign_in" method="post">
-                <div className="field">
-                  <label htmlFor="admin_identifier">Username or Email</label><br />
-                  <input
-                    autoFocus
-                    type="text"
-                    name="identifier"
-                    id="admin_identifier"
-                    value={formData.identifier}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <div className="field">
-                  <label htmlFor="admin_password">Password</label><br />
-                  <input
-                    type="password"
-                    name="password"
-                    id="admin_password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                  />
-                </div>
-                <div className="field checkbox-field">
-                  <input
-                    type="checkbox"
-                    name="rememberMe"
-                    id="admin_remember_me"
-                    checked={formData.rememberMe}
-                    onChange={handleInputChange}
-                  />
-                  <label htmlFor="admin_remember_me">Remember me</label>
-                </div>
+                {/* ... (unchanged code) ... */}
                 <div className="actions">
                   <button type="button" onClick={handleLogin} className="btn btn-primary">
                     Log in
