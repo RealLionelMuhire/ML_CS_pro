b9a2c3433e7ffef9e75e18743206e873fafd16eb
RealLionelMuhire <muhirelionel@gmail.com>
2023-11-19 20:22:22 +0200

    adding admin models

--- backend/app/models/admin.py
+++ backend/app/models/admin.py
@@ -1,13 +1,32 @@
-from app import db
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
-class Administrator(db.Model):
-    __tablename__ = 'administrators'
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
-    role = db.Column(db.String(50), nullable=False)  # Assuming roles like 'Level 1', 'Level 2', 'Level 3'
-    # Add other fields as per your requirements, e.g., name, email, etc.
+class User(db.Model):
+    __tablename__ = 'Users'
 
-    def __repr__(self):
-        return f"<Administrator {self.username}>"
+    UserID = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    UserType = db.Column(db.String(20), nullable=False)
+    Username = db.Column(db.String(50), nullable=False, unique=True)
+    Password = db.Column(db.String(255), nullable=False)
+    Email = db.Column(db.String(50), nullable=False, unique=True)
+    FullName = db.Column(db.String(255), nullable=False)
+    NationalID = db.Column(db.String(25), nullable=False, unique=True)
+    Location = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, UserType, Username, Password, Email, FullName, NationalID, Location):
+        self.UserType = UserType
+        self.Username = Username
+        self.Password = self.hash_password(Password)  # Hash the password before storing it
+        self.Email = Email
+        self.FullName = FullName
+        self.NationalID = NationalID
+        self.Location = Location
+
+    def hash_password(self, password):
+        return bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.Password, password)
