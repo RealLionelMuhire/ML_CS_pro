6b8901bffac86493dc2b5070ee25bf2e6b33e30d
RealLionelMuhire <muhirelionel@gmail.com>
2023-11-19 22:18:45 +0200

    adding something on jsonify

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,5 +1,5 @@
-# app/__init__.py
-from flask import Flask
+# App/__init__.py
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from config.app_config import AppConfig
@@ -7,16 +7,27 @@ from config.app_config import AppConfig
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
-# this is of creatin and inirialising the flask app
 def create_app():
     app = Flask(__name__)
     app.config.from_object(AppConfig)
 
+    # Initialize extensions
     db.init_app(app)
     bcrypt.init_app(app)
 
+    # Register blueprints
     from app.routes.admin_routes import admin_routes
-    app.register_blueprint(admin_routes)
+    app.register_blueprint(admin_routes, url_prefix='/admin')  # Adjust the prefix as needed
 
+    # Add more blueprints as your application grows
+
+    # Error handling
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return jsonify({'error': 'Resource not found'}), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return jsonify({'error': 'Internal server error'}), 500
 
     return app
