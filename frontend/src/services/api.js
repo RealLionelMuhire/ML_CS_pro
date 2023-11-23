e5cecadda77b6fdb1282f809e04dff645500fb6a
RealLionelMuhire <muhirelionel@gmail.com>
2023-11-23 11:09:26 +0200

    adding some csrf token and handling in in authentication

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,27 @@
 const BASE_URL = 'http://127.0.0.1:8000';
 
+// Function to get the CSRF token from cookies
+const getCsrfToken = () => {
+  const name = 'csrftoken';
+  const cookieValue = document.cookie.match(`(^|;)\\s*${name}\\s*=\\s*([^;]+)`);
+  return cookieValue ? cookieValue.pop() : '';
+};
+
+// Function to get the authentication token from your application state or any storage
+const getAuthToken = () => {
+  // Replace this with the actual code to get the authentication token from your state or storage
+  // Example: return yourAuthTokenState;
+  return '';
+};
+
 const registerUser = async (userData) => {
   try {
     const response = await fetch(`${BASE_URL}/api/register/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'X-CSRFToken': getCsrfToken(),
+        'Authorization': `Token ${getAuthToken()}`, // Include the authentication token
       },
       body: JSON.stringify(userData),
     });
@@ -30,6 +46,8 @@ const loginUser = async (loginData) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'X-CSRFToken': getCsrfToken(),
+        'Authorization': `Token ${getAuthToken()}`, // Include the authentication token
       },
       body: JSON.stringify(loginData),
     });
