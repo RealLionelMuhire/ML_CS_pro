5b386195e3c83c7151d2711bdbe3049370b8b991
RealLionelMuhire <muhirelionel@gmail.com>
2023-11-23 15:13:14 +0200

    adding field config related to the backend

--- frontend/src/components/Authentication/AdminAuthentication/AdminRegistrationPage.js
+++ frontend/src/components/Authentication/AdminAuthentication/AdminRegistrationPage.js
@@ -11,9 +11,10 @@ function AdminRegistrationPage() {
     password: '',
     confirmPassword: '',
     email: '',
-    fullName: '',
-    nationalID: '',
-    location: '',
+    FullName: '',
+    NationalID: '',
+    Location: '',
+    UserType: '',
   });
 
   const [passwordError, setPasswordError] = useState('');
@@ -26,29 +27,31 @@ function AdminRegistrationPage() {
 
   const validatePassword = () => {
     const { password, confirmPassword } = formData;
-  
+
     // Password strength requirements
     const strongRegex = new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})');
-  
+
     if (!strongRegex.test(password)) {
-      setPasswordError('Password must include uppercase, lowercase, numbers, special characters, and be at least 8 characters long');
+      setPasswordError(
+        'Password must include uppercase, lowercase, numbers, special characters, and be at least 8 characters long'
+      );
     } else {
       setPasswordError('');
     }
-  
+
     if (password !== confirmPassword) {
       setPasswordError("Passwords don't match");
     } else {
       setPasswordError(''); // Clear the error if passwords match
     }
-  
+
     if (password.length > 0 && password.length < 8) {
       setPasswordSuggestions('Consider a password 8 Characters, or longer for better security.');
     } else {
       setPasswordSuggestions('');
     }
   };
-  
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -61,9 +64,10 @@ function AdminRegistrationPage() {
           username: formData.username,
           password: formData.password,
           email: formData.email,
-          fullName: formData.fullName,
-          nationalID: formData.nationalID,
-          location: formData.location,
+          FullName: formData.FullName, // Update to match the expected case
+          NationalID: formData.NationalID, // Update to match the expected case
+          Location: formData.Location, // Update to match the expected case
+          UserType: formData.UserType || 'DefaultUserType', // Default value for userType
         });
 
         // Handle the response (e.g., display a success message, redirect, etc.)
@@ -87,7 +91,18 @@ function AdminRegistrationPage() {
                 {Object.keys(formData).map((fieldName) => (
                   <div key={fieldName} className="field">
                     <label htmlFor={fieldName}>{fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}:</label><br />
-                    {fieldName.includes('password') ? (
+                    {fieldName === 'UserType' ? (
+                      <select
+                        name="UserType"
+                        value={formData.userType}
+                        onChange={handleChange}
+                      >
+                        <option value="">Select User Type</option>
+                        <option value="Level 1 Admin">Level 1 Admin</option>
+                        <option value="Level 2 Admin">Level 2 Admin</option>
+                        <option value="Level 3 Admin">Level 3 Admin</option>
+                      </select>
+                    ) : fieldName.includes('password') ? (
                       <>
                         <input
                           type="password"
