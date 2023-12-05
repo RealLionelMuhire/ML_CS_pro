02faf57eb02931db1c5131ec239ea009c4eb6d38
RealLionelMuhire <muhirelionel@gmail.com>
2023-12-05 22:10:03 +0200

    adding some collapsables

--- frontend/src/components/AdminAccounts/AdminDashboardPage.js
+++ frontend/src/components/AdminAccounts/AdminDashboardPage.js
@@ -1,106 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../Common/Header';
 import Footer from '../Common/Footer';
 import './AdminDashboardPage.css';
 
 function AdminDashboardPage() {
+  const [collapsedOptions, setCollapsedOptions] = useState({});
+
+  const toggleCollapse = (option) => {
+    setCollapsedOptions((prevCollapsedOptions) => ({
+      ...prevCollapsedOptions,
+      [option]: !prevCollapsedOptions[option],
+    }));
+  };
+
+  const options = [
+    { path: '/find-client', icon: 'search', text: 'Find Client Record' },
+    { path: '/active-clients', icon: 'person', text: 'Active Clients' },
+    { path: '/appointment-scheduling', icon: 'calendar_today', text: 'Appointment Scheduling' },
+    { path: '/register-client', icon: 'person_add', text: 'Register a Client' },
+    { path: '/work-on-client-documents', icon: 'description', text: 'Work on Client Documents' },
+    { path: '/reports', icon: 'description', text: 'Reports' },
+    { path: '/set-schedule', icon: 'schedule', text: 'Set a Schedule' },
+    { path: '/data-management', icon: 'storage', text: 'Data Management' },
+    { path: '/configure-metadata', icon: 'settings', text: 'Configure Metadata' },
+    { path: '/system-administration', icon: 'build', text: 'System Administration' },
+    { path: '/messaging', icon: 'message', text: 'Messaging' },
+    { path: '/view-logs-activities', icon: 'history', text: 'View Logs and Activities' },
+    { path: '/alerts', icon: 'notifications', text: 'Alerts' },
+    { path: '/activate-deactivate', icon: 'power_settings_new', text: 'Activate and Deactivate the Client' },
+    { path: '/add-review-suggestion', icon: 'rate_review', text: 'Add a Review or Suggestion for the App' },
+  ];
+
   return (
     <div className="dashboard-page">
       <Header />
       <main>
         <div className="container">
           <div className="dashboard">
-            <Link to="/find-client">
-              <div className="option">
-                <i className="material-icons">search</i>
-                <br />Find Client Record
-              </div>
-            </Link>
-            <Link to="/active-clients">
-              <div className="option">
-                <i className="material-icons">person</i>
-                <br />Active Clients
-              </div>
-            </Link>
-            <Link to="/appointment-scheduling">
-              <div className="option">
-                <i className="material-icons">calendar_today</i>
-                <br />Appointment Scheduling
-              </div>
-            </Link>
-            <Link to="/register-client">
-              <div className="option">
-                <i className="material-icons">person_add</i>
-                <br />Register a Client
-              </div>
-            </Link>
-            <Link to="/work-on-client-documents">
-              <div className="option">
-                <i className="material-icons">description</i>
-                <br />Work on Client Documents
-              </div>
-            </Link>
-            <Link to="/reports">
-              <div className="option">
-                <i className="material-icons">description</i>
-                <br />Reports
-              </div>
-            </Link>
-            <Link to="/set-schedule">
-              <div className="option">
-                <i className="material-icons">schedule</i>
-                <br />Set a Schedule
-              </div>
-            </Link>
-            <Link to="/data-management">
-              <div className="option">
-                <i className="material-icons">storage</i>
-                <br />Data Management
-              </div>
-            </Link>
-            <Link to="/configure-metadata">
-              <div className="option">
-                <i className="material-icons">settings</i>
-                <br />Configure Metadata
-              </div>
-            </Link>
-            <Link to="/system-administration">
-              <div className="option">
-                <i className="material-icons">build</i>
-                <br />System Administration
-              </div>
-            </Link>
-            <Link to="/messaging">
-              <div className="option">
-                <i className="material-icons">message</i>
-                <br />Messaging
-              </div>
-            </Link>
-            <Link to="/view-logs-activities">
-              <div className="option">
-                <i className="material-icons">history</i>
-                <br />View Logs and Activities
-              </div>
-            </Link>
-            <Link to="/alerts">
-              <div className="option">
-                <i className="material-icons">notifications</i>
-                <br />Alerts
-              </div>
-            </Link>
-            <Link to="/activate-deactivate">
-              <div className="option">
-                <i className="material-icons">power_settings_new</i>
-                <br />Activate and Deactivate the Client
-              </div>
-            </Link>
-            <Link to="/add-review-suggestion">
-              <div className="option">
-                <i className="material-icons">rate_review</i>
-                <br />Add a Review or Suggestion for the App
-              </div>
-            </Link>
+            {options.map((option) => (
+              <Link to={option.path} key={option.path}>
+                <div
+                  className={`option ${collapsedOptions[option.path] ? 'collapsed' : ''}`}
+                  onClick={() => toggleCollapse(option.path)}
+                >
+                  <i className="material-icons">{option.icon}</i>
+                  <br />
+                  {!collapsedOptions[option.path] && option.text}
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </main>
