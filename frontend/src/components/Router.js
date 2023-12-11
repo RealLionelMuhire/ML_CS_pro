638ed6e1bebfbeb70a9cf51ef5dca13ad070aaf0
Janvier Habineza <janvierhabineza@Janviers-MacBook-Pro.local>
2023-12-11 04:48:00 +0200

    add the admin dashboard

--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import HomePage from './HomePage';
-import ClientLoginPage from './Authentication/ClientAuthentication/ClientLoginPage';
-import ClientRegistrationPage from './Authentication/ClientAuthentication/ClientRegistrationPage';
-import AdminLoginPage from './Authentication/AdminAuthentication/AdminLoginPage';
-import AdminRegistrationPage from './Authentication/AdminAuthentication/AdminRegistrationPage';
-import AdminDashboardPage from './AdminAccounts/AdminDashboardPage';
-import ClientDashboardPage from './ClientAccounts/ClientDashboardPage';
-import ClientAcceptanceForm1 from './ClientAccounts/ClientForms/ClientForm1/ClientAcceptanceForm1';
-import ClientList from './ClientData/ClientList';
-import FindClientComponent from './AdminAccounts/FindClientComponent';
-import ActiveClientsComponent from './AdminAccounts/ActiveClientsComponent';
-import AppointmentSchedulingComponent from './AdminAccounts/AppointmentSchedulingComponent';
-import RegisterClientComponent from './AdminAccounts/RegisterClientComponent';
-import ProtectedRoute from './ProtectedRoute';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./HomePage";
+import ClientLoginPage from "./Authentication/ClientAuthentication/ClientLoginPage";
+import ClientRegistrationPage from "./Authentication/ClientAuthentication/ClientRegistrationPage";
+import AdminLoginPage from "./Authentication/AdminAuthentication/AdminLoginPage";
+import AdminRegistrationPage from "./Authentication/AdminAuthentication/AdminRegistrationPage";
+import AdminDashboardPage from "./AdminAccounts/AdminDashboardPage";
+import ClientDashboardPage from "./ClientAccounts/ClientDashboardPage";
+import ClientAcceptanceForm1 from "./ClientAccounts/ClientForms/ClientForm1/ClientAcceptanceForm1";
+import ClientList from "./ClientData/ClientList";
+import FindClientComponent from "./AdminAccounts/FindClientComponent";
+import ActiveClientsComponent from "./AdminAccounts/ActiveClientsComponent";
+import AppointmentSchedulingComponent from "./AdminAccounts/AppointmentSchedulingComponent";
+import RegisterClientComponent from "./AdminAccounts/RegisterClientComponent";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppRouter() {
   return (
@@ -22,19 +22,26 @@ function AppRouter() {
       <Route path="/client-login" element={<ClientLoginPage />} />
       <Route path="/client-registration" element={<ClientRegistrationPage />} />
       <Route path="/client-dashboard" element={<ClientDashboardPage />} />
-      <Route path="/client-acceptance-form" element={<ClientAcceptanceForm1 />} />
+      <Route
+        path="/client-acceptance-form"
+        element={<ClientAcceptanceForm1 />}
+      />
       <Route path="/admin-login" element={<AdminLoginPage />} />
       <Route path="/admin-registration" element={<AdminRegistrationPage />} />
       {/* <Route path="/client-list" element={<ClientList />} /> */}
       <Route path="/find-client" element={<FindClientComponent />} />
       <Route path="/active-clients" element={<ActiveClientsComponent />} />
-      <Route path="/appointment-scheduling" element={<AppointmentSchedulingComponent />} />
+      <Route
+        path="/appointment-scheduling"
+        element={<AppointmentSchedulingComponent />}
+      />
       <Route path="/register-client" element={<RegisterClientComponent />} />
+      <Route path="//admin-dashboard" element={<AdminDashboardPage />} />
 
       {/* Nested Routes under /admin-dashboard */}
-      <Route path="/admin-dashboard/*" element={<ProtectedRoute />}>
+      {/* <Route path="/admin-dashboard/*" element={<ProtectedRoute />}>
         <Route index element={<AdminDashboardPage />} />
-      </Route>
+      </Route> */}
     </Routes>
   );
 }
